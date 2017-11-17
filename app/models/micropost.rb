2122be44fc2bc06bf8a89bd934a52806d64e8569
Bui Huy Hoang <hoangbui237@gmail.com>
2017-11-17 16:50:26 +0700

    Add model and relationships

---
+++ app/models/micropost.rb
@@ -0,0 +1,10 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  validates :user_id, presence: true
+  validates :title, presence: true, length: {maximum:Settings.post_max_length_title}
+  validates :content, presence: true, length: {maximum:Settings.post_max_length_content}
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+end
